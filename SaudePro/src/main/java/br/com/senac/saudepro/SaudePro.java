65893dc593b9226dafa14770d076922f26cbdfc7
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2026-04-18 13:58:57 -0400

    feat(main): adiciona classe principal com inicialização do sistema SaúdePro
    - Look and Feel Nimbus para interface moderna
    - Inicialização thread-safe da tela de login
    - Integração View (Login) e Controller (LoginController)

--- SaudePro/src/main/java/br/com/senac/saudepro/SaudePro.java
+++ SaudePro/src/main/java/br/com/senac/saudepro/SaudePro.java
@@ -1,13 +1,44 @@
 package br.com.senac.saudepro;
 
+import br.com.senac.saudepro.controller.LoginController;
+import br.com.senac.saudepro.gui.Login;
+
 /**
- *
+ * Class principal inicar o programa
  * @author bruno-teixeira
  */
 public class SaudePro {
+     // remover apos testes
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Login.class.getName());
 
     public static void main(String[] args) {
         // ira rodar a gui principal
+        viewLogin();
         
+    }
+    
+    
+    private static void viewLogin(){
+             try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            Login l = new Login();
+            
+            new LoginController(l);
+            
+            
+            l.setVisible(true);
+            
+                    
+        });
     }
 }
